00f57cb7b4237e441f4400ad99a823d6d5db2543
moazzamsadiq <moazzam@syntaxtechs.com>
2022-10-02 13:32:52 -0400

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 	for (int i =0 ; i<5 ;i++){
 
-        System.out.println("zameer is zombie");
+        System.out.println("hello there how are you");
         Thread.sleep(3000);
 
     }
