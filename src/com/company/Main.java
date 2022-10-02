fc354990e5e6a63f1b6a1e44551db75cb9203519
moazzamsadiq <moazzam@syntaxtechs.com>
2022-10-02 13:06:28 -0400

    1st commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	for (int i =0 ; i<5 ;i++){
+
+        System.out.println("zameer is zombie");
+        Thread.sleep(3000);
+
+    }
+    }
+}
